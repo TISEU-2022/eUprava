01af68f7e4ba3ca3e5c3a3fc29925a2ea33d1137
Ivan Djuraki <ivandjuraki@protonmail.com>
2022-04-02 15:10:00 +0200

    maticar: admin panel wip

--- 4. maticar/frontend/src/components/admin/AdminPanel.tsx
+++ 4. maticar/frontend/src/components/admin/AdminPanel.tsx
@@ -1,13 +1,47 @@
-import React, { useEffect } from 'react';
+import { Box, Flex } from '@chakra-ui/react';
+import React, { useEffect, useState } from 'react';
 import { getWorkers } from '../../api/api';
+import CommonDataTable, { CommonDataTableHeadings } from '../common/CommonDataTable';
 
 const AdminPanel = () => {
+  const [workers, setWorkers] = useState([]);
+
+  const columns = [
+    {
+      key: 'firstName',
+      numeric: false,
+      title: 'First name'
+    },
+    {
+      key: 'lastName',
+      numeric: false,
+      title: 'Last name'
+    },
+    {
+      key: 'identityNumber',
+      numeric: false,
+      title: 'Identity Number'
+    },
+    {
+      key: 'username',
+      numeric: false,
+      title: 'Username'
+    }]
+
   useEffect(() => {
     getWorkers().then((res) => {
-      console.log(res?.data);
+      setWorkers(res?.data)
     });
   }, []);
-  return <div>AdminPanel</div>;
+
+  return <Box mt={20} ml={50} mr={50}>
+    <CommonDataTable
+      data={workers}
+      searchInputProps={{width: 200, m: 2}}
+      onRowClickHandler={(value) => {console.log(value)}}
+      tableHeadings={columns}
+    />
+  </Box>
 };
 
 export default AdminPanel;
