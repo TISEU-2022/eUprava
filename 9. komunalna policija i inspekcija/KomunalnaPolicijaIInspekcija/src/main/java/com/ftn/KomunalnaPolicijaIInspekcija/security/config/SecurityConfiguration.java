0adf94c997027ec927681eba0430f30103edc677
Branimir <tomicbranimir00@gmail.com>
2022-04-27 01:30:44 +0200

    Add security and connection to auth server

---
+++ 9. komunalna policija i inspekcija/KomunalnaPolicijaIInspekcija/src/main/java/com/ftn/KomunalnaPolicijaIInspekcija/security/config/SecurityConfiguration.java
@@ -0,0 +1,53 @@
+package com.ftn.KomunalnaPolicijaIInspekcija.security.config;
+
+import com.ftn.KomunalnaPolicijaIInspekcija.security.rest.RestAuthenticationEntryPoint;
+import com.ftn.KomunalnaPolicijaIInspekcija.security.token.TokenAuthenticationFilter;
+import com.ftn.KomunalnaPolicijaIInspekcija.security.token.TokenUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private RestAuthenticationEntryPoint restAuthenticationEntryPoint;
+
+    @Autowired
+    private TokenUtils tokenUtils;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.headers().cacheControl().disable();
+        httpSecurity
+                .cors().and()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                .exceptionHandling().authenticationEntryPoint(restAuthenticationEntryPoint).and()
+				.authorizeRequests()
+                    .anyRequest().permitAll().and()
+                .addFilterBefore(new TokenAuthenticationFilter(tokenUtils), BasicAuthenticationFilter.class);
+    }
+}
