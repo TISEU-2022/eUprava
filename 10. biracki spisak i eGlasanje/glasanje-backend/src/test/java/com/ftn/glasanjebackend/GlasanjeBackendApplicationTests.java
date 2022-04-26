6ed8567e215fe2af6add81046a49f48983a644a6
LukaZdjelar <62383774+LukaZdjelar@users.noreply.github.com>
2022-04-26 16:47:06 +0200

    Backend initialization

---
+++ 10. biracki spisak i eGlasanje/glasanje-backend/src/test/java/com/ftn/glasanjebackend/GlasanjeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ftn.glasanjebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlasanjeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
