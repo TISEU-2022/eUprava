320b4afd197f02cc85b8f07d25b37301418233bc
Nemanja Nikolic <nemanja123444@gmail.com>
2022-04-26 22:20:55 +0200

    Added Backend

---
+++ 13. predskolske ustanove/predskolsko/src/main/java/com/projekat/predskolsko/model/Roditelj.java
@@ -0,0 +1,149 @@
+package com.projekat.predskolsko.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "roditelj")
+public class Roditelj {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "roditelj_id", unique = true, nullable = false)
+	private Integer roditelj_id;
+
+	@Column(name = "name", unique = false, nullable = false)
+	private String name;
+
+	@Column(name = "lastname", unique = false, nullable = false)
+	private String lastname;
+
+	@Column(name = "username", unique = true, nullable = false)
+	private String username;
+
+	@Column(name = "password", unique = false, nullable = false)
+	private String password;
+
+	@Column(name = "address", unique = false, nullable = false)
+	private String address;
+
+	@Column(name = "jmbg", unique = true, nullable = false)
+	private String jmbg;
+	
+	@Column(name = "placeOfBirth", unique = false, nullable = false)
+	private String placeOfBirth;
+	
+	@Column(name = "gender", unique = false, nullable = false)
+	private boolean gender;
+
+	@Column(name = "blocked", unique = false, nullable = false)
+	private boolean blocked;
+	
+	
+
+	public Roditelj() {
+		super();
+	}
+
+	public Roditelj(Integer roditelj_id, String name, String lastname, String username, String password, String address,
+			String jmbg, String placeOfBirth, boolean gender, boolean blocked) {
+		super();
+		this.roditelj_id = roditelj_id;
+		this.name = name;
+		this.lastname = lastname;
+		this.username = username;
+		this.password = password;
+		this.address = address;
+		this.jmbg = jmbg;
+		this.placeOfBirth = placeOfBirth;
+		this.gender = gender;
+		this.blocked = blocked;
+	}
+
+
+
+	public Integer getRoditelj_id() {
+		return roditelj_id;
+	}
+
+	public void setRoditelj_id(Integer roditelj_id) {
+		this.roditelj_id = roditelj_id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getJmbg() {
+		return jmbg;
+	}
+
+	public void setJmbg(String jmbg) {
+		this.jmbg = jmbg;
+	}
+
+	public String getPlaceOfBirth() {
+		return placeOfBirth;
+	}
+
+	public void setPlaceOfBirth(String placeOfBirth) {
+		this.placeOfBirth = placeOfBirth;
+	}
+
+	public boolean isGender() {
+		return gender;
+	}
+
+	public void setGender(boolean gender) {
+		this.gender = gender;
+	}
+
+	public boolean isBlocked() {
+		return blocked;
+	}
+
+	public void setBlocked(boolean blocked) {
+		this.blocked = blocked;
+	}
+	
+	
+}
