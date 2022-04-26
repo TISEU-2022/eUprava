95cdb7b5059831c8a574cbcd66d71cd8f61307e2
ljubomirlujic <ljubomir.lujic60@gmail.com>
2022-04-26 20:57:39 +0200

    Spring boot and mysql dockerfile and compose

--- 5. zdravstvo/zdravstvo/src/main/java/ftn/euprava/zdravstvo/api/ZdravstvoController.java
+++ 5. zdravstvo/zdravstvo/src/main/java/ftn/euprava/zdravstvo/api/ZdravstvoController.java
@@ -1,30 +1,17 @@
 package ftn.euprava.zdravstvo.api;
 
-import ftn.euprava.zdravstvo.api.dto.UverenjeDTO;
-import ftn.euprava.zdravstvo.api.dto.UverenjeZahtevDTO;
-import ftn.euprava.zdravstvo.model.Termin;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
-@Controller("/api")
+@RestController
+@RequestMapping("/api/zdravstvo")
 @CrossOrigin("*")
 public class ZdravstvoController {
 
-
-
-
-    @GetMapping("/medical-certificate")
-    public UverenjeDTO getLekarskoUverenje(@RequestBody()UverenjeZahtevDTO uverenjeZahtevDTO){
-        UverenjeDTO uverenjeDTO = new UverenjeDTO();
-        return uverenjeDTO;
-    }
-
     @GetMapping("/appointment-history")
-    public List<Termin> getHistory(){
-        return null;
+    public ResponseEntity getHistory(){
+        return new ResponseEntity(HttpStatus.OK);
     }
 }
