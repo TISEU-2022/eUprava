b478906f94c89bf64c30aab01639740a1be5bf5e
Nikoa <nikola.kiki.00@gmail.com>
2022-04-26 14:57:06 +0200

    Initial Backend and Dockerfile commit

---
+++ 3. nacionalna sluzba za zaposljavanje/backend/SluzbaZaposljavanja/src/main/java/com/example/SluzbaZaposljavanja/SluzbaZaposljavanjaApplication.java
@@ -0,0 +1,13 @@
+package com.example.SluzbaZaposljavanja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SluzbaZaposljavanjaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SluzbaZaposljavanjaApplication.class, args);
+	}
+
+}
---
+++ 3. nacionalna sluzba za zaposljavanje/backend/SluzbaZaposljavanja/src/main/java/com/example/SluzbaZaposljavanja/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.example.SluzbaZaposljavanja.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "test")
+public class TestController {
+
+    @GetMapping
+    public ResponseEntity<String> ispisiSve(){
+        return new ResponseEntity<>("hello", HttpStatus.ACCEPTED);
+    }
+}
