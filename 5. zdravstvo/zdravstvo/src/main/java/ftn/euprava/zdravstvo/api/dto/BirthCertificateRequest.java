450dd8d1f977b110e75839fd5709563ddcb9fd38
Jelena Mihajlovic <starglitz47@gmail.com>
2022-05-14 17:49:09 +0200

    add dto validation

--- 5. zdravstvo/zdravstvo/src/main/java/ftn/euprava/zdravstvo/api/dto/BirthCertificateRequest.java
+++ 5. zdravstvo/zdravstvo/src/main/java/ftn/euprava/zdravstvo/api/dto/BirthCertificateRequest.java
@@ -4,6 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @NoArgsConstructor
@@ -11,14 +14,35 @@ import java.util.Date;
 @Data
 public class BirthCertificateRequest {
 
+    @NotNull
+    @NotEmpty
+    @Size(min=13, max=13)
     private String identificationNumber;
+    @NotNull
+    @NotEmpty
     private String firstName;
+    @NotNull
+    @NotEmpty
     private String lastName;
+    @NotNull
+    @NotEmpty
     private String gender;
+    @NotNull
+    @NotEmpty
     private Date dateOfBirth;
+    @NotNull
+    @NotEmpty
     private String countryOfBirth;
+    @NotNull
+    @NotEmpty
     private String citizenship;
+    @NotNull
+    @NotEmpty
+    @Size(min=13, max=13)
     private String parent1Id;
+    @NotNull
+    @NotEmpty
+    @Size(min=13, max=13)
     private String parent2Id;
 
 
