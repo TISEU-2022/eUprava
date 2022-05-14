d52b57bf16732a74c3e636b3e3103b1a72507ca2
Jelena Mihajlovic <60920171+starglitz@users.noreply.github.com>
2022-05-14 12:34:26 +0200

    save user who requested certificate to system & add separate endpoint to check if he has certificate (#60)

---
+++ 5. zdravstvo/zdravstvo/src/main/java/ftn/euprava/zdravstvo/model/User.java
@@ -0,0 +1,19 @@
+package ftn.euprava.zdravstvo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+}
