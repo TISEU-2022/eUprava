50bf24845d8c5e60ebd49becb0daee727eee5021
Nikoa <nikola.kiki.00@gmail.com>
2022-05-26 00:16:26 +0200

    Fixed conflict, added frontend base and functionality

--- 3. nacionalna sluzba za zaposljavanje/backend/SluzbaZaposljavanja/src/main/java/com/example/SluzbaZaposljavanja/controller/GradjaninController.java
+++ 3. nacionalna sluzba za zaposljavanje/backend/SluzbaZaposljavanja/src/main/java/com/example/SluzbaZaposljavanja/controller/GradjaninController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
+@CrossOrigin(origins="http://localhost:3000")
 @RequestMapping(value = "api/gradjani")
 public class GradjaninController {
 
--- 3. nacionalna sluzba za zaposljavanje/backend/SluzbaZaposljavanja/src/main/java/com/example/SluzbaZaposljavanja/controller/KonkursController.java
+++ 3. nacionalna sluzba za zaposljavanje/backend/SluzbaZaposljavanja/src/main/java/com/example/SluzbaZaposljavanja/controller/KonkursController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,8 @@ import java.util.List;
 
 @Controller
 @RequestMapping(value = "api/konkursi")
+@CrossOrigin(origins="http://localhost:3000")
+
 public class KonkursController {
 
     @Autowired
