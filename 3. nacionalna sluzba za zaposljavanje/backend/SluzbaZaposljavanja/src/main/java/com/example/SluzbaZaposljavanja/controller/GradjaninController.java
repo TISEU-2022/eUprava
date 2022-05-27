fe3be202fddd7888b87d19da937a3b05a7c0dc7a
Nikola Smiljanic <62019267+SmiljanicNikola@users.noreply.github.com>
2022-05-27 21:02:21 +0200

    Zaposljavanje init (#59)
    * Initial Backend and Dockerfile commit
    
    * Finished model, added Repos, Services and some Controllers
    
    * Implemented all controllers and Get methods for each entity
    
    * Added new functionality
    
    * save user who requested certificate to system & add separate endpoint to check if he has certificate (#60)
    
    * Tim11 patch#8 (#58)
    
    * --Backend init
    
    * --Tweaks to the Dockerfile
    
    * --Tweaks to the Dockerfile v2
    
    * --Tweaks to the Dockerfile v2
    
    Implemented the necessary changes, .jar file is now built with maven
    
    * --Tweaks to the Dockerfile v2
    
    Implemented the necessary changes, .jar file is now built with maven
    
    * Add model, repository
    
    Classes for User and Document models added, implemented JPA
    
    * Add frontend project
    
    --Initiated a React project for the frontend, dockerfile included
    
    * Update docker compose
    
    * Tim11 patch#9 (#61)
    
    * --Backend init
    
    * --Tweaks to the Dockerfile
    
    * --Tweaks to the Dockerfile v2
    
    * --Tweaks to the Dockerfile v2
    
    Implemented the necessary changes, .jar file is now built with maven
    
    * --Tweaks to the Dockerfile v2
    
    Implemented the necessary changes, .jar file is now built with maven
    
    * Add model, repository
    
    Classes for User and Document models added, implemented JPA
    
    * Add frontend project
    
    --Initiated a React project for the frontend, dockerfile included
    
    * Update docker compose
    
    * Add CRUD operations for Documents
    
    Dockercompose file has been altered according to previous suggestions.
    
    * Fixed conflict, added frontend base and functionality
    
    * Fixed
    
    * Fixed coflict
    
    Co-authored-by: Jelena Mihajlovic <60920171+starglitz@users.noreply.github.com>
    Co-authored-by: foabnesa99 <64659134+foabnesa99@users.noreply.github.com>

--- 3. nacionalna sluzba za zaposljavanje/backend/SluzbaZaposljavanja/src/main/java/com/example/SluzbaZaposljavanja/controller/GradjaninController.java
+++ 3. nacionalna sluzba za zaposljavanje/backend/SluzbaZaposljavanja/src/main/java/com/example/SluzbaZaposljavanja/controller/GradjaninController.java
@@ -1,5 +1,6 @@
 package com.example.SluzbaZaposljavanja.controller;
 
+import com.example.SluzbaZaposljavanja.model.Firma;
 import com.example.SluzbaZaposljavanja.model.Gradjanin;
 import com.example.SluzbaZaposljavanja.model.OglasZaPosao;
 import com.example.SluzbaZaposljavanja.service.GradjaninService;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
+@CrossOrigin(origins="http://localhost:3000")
 @RequestMapping(value = "api/gradjani")
 public class GradjaninController {
 
@@ -38,6 +40,22 @@ public class GradjaninController {
     public ResponseEntity<Gradjanin> save(@RequestBody Gradjanin gradjanin){
         Gradjanin newGradjanin = gradjaninService.save(gradjanin);
         return ResponseEntity.status(201).body(newGradjanin);
+    }
+
+    @GetMapping(value = "zaposlenje/{korisnickoIme}")
+    public ResponseEntity<Boolean> getStatusZaposlenjaGradjanina(@PathVariable("korisnickoIme") String korisnickoIme){
+        Gradjanin gradjanin = gradjaninService.findByKorisnickoIme(korisnickoIme);
+        Firma firma = gradjanin.getFirma();
+        boolean zaposlenje;
+        if(firma == null) {
+            zaposlenje = false;
+        }else{
+            zaposlenje = true;
+        }
+        if(gradjanin == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<Boolean>(zaposlenje, HttpStatus.OK);
     }
 
 }
