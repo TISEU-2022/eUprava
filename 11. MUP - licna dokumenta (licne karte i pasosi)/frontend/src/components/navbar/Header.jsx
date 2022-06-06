f297b13fe0303eeaf3f09f4ccad015dc2cfbe616
Aleksa Vučenović <aleksavucenovic1@gmail.com>
2022-06-06 20:20:09 +0200

    Functions done: Home page, Routing. (#66)
    Connecting to the auth server: Login, Register, Logout.
    These changes are on the front side

---
+++ 11. MUP - licna dokumenta (licne karte i pasosi)/frontend/src/components/navbar/Header.jsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Navbar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
+import Button from 'react-bootstrap/Button'
+import axios from 'axios';
+import { useJwt } from "react-jwt";
+
+export const Header = () => {
+    const navigate = useNavigate();
+
+    const [user, setUser] = useState(false)
+    const [username, setUsername] = useState(false)
+    const usertoken = localStorage.getItem("token")
+    const { decodedToken, isExpired } = useJwt(usertoken);
+
+    const logOut = () => {
+        window.open(
+            `http://localhost:5101/auth/logout?successUrl=http://localhost:11000/auth/logout_handler`,
+            '_self',
+        );
+    }
+
+    return (
+        <>
+            <Navbar bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand href="/">MUP - Licna dokumenta (Tim 11)</Navbar.Brand>
+                    <Navbar.Toggle />
+                    <Navbar.Collapse className="justify-content-end">
+                        {decodedToken && decodedToken.username && !isExpired &&
+                            <>
+                                <Navbar.Text>
+                                    Signed in as: <a href="#">{decodedToken.username}</a>
+                                </Navbar.Text>
+                                <Nav.Link onClick={() => logOut()}>Logout</Nav.Link>
+                            </>
+                        }
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </>
+    )
+}
+
+export default Header
