b4326ae93bacb697c6caaca63e26e2df9eaaf2d9
ljubomirlujic <ljubomir.lujic60@gmail.com>
2022-06-10 13:08:56 +0200

    Made issue medical-certificaton api available to all users

--- 5. zdravstvo/zdravstvo/src/main/java/ftn/euprava/zdravstvo/security/SecurityConfiguration.java
+++ 5. zdravstvo/zdravstvo/src/main/java/ftn/euprava/zdravstvo/security/SecurityConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -49,9 +50,13 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("/h2-console/**").permitAll()
                 .antMatchers( "*").permitAll()
                 .antMatchers( "*").permitAll()
+                .antMatchers(HttpMethod.POST, "/api/medical-certificates").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/medical-certificates/*").permitAll()
                 .anyRequest().authenticated();
 
         httpSecurity.addFilterBefore(authenticationTokenFilterBean(), UsernamePasswordAuthenticationFilter.class);
     }
+
+
 }
 
