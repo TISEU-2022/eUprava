3a806b65fe5e18e5c11681b7419f7ae26d4402a5
lukamiljus123 <62016060+lukamiljus123@users.noreply.github.com>
2022-06-17 14:04:55 +0200

    aktuelni izbori button

--- 10. biracki spisak i eGlasanje/glasanje-frontend/src/components/NavigationBar.js
+++ 10. biracki spisak i eGlasanje/glasanje-frontend/src/components/NavigationBar.js
@@ -6,25 +6,25 @@ export default class NavigationBar extends React.Component {
     render() {
         return (
             <Navbar className="navigacija" bg="dark" variant="dark">
-                <a className="btn btn-primary"
-                   href={
-                       localStorage.getItem("id") == null
-                           ?
-                           "/prijava"
-                           :
-                           "/"
-                   }
-                   role="button">
-                    Почетна
-                </a>
                 {
                     localStorage.getItem("id") == null
                         ?
                         <b/>
                         :
                         <a className="btn btn-primary"
-                            href={"/zavrseniIzbori"}
-                            role="button">
+                           href={"/"}
+                           role="button">
+                            Актуелни избори
+                        </a>
+                }
+                {
+                    localStorage.getItem("id") == null
+                        ?
+                        <b/>
+                        :
+                        <a className="btn btn-primary"
+                           href={"/zavrseniIzbori"}
+                           role="button">
                             Завршени избори
                         </a>
                 }
@@ -34,7 +34,7 @@ export default class NavigationBar extends React.Component {
                         <b/>
                         :
                         <a className="btn btn-primary"
-                           href="http://localhost:4101/auth/logout?successUrl=http://localhost:10001/prijava"
+                           onClick={() => AuthenticationService.logout()}
                            role="button">
                             Одјава
                         </a>
